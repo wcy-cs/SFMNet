3dabf9c50ed2aa31b5b191d996705f9b6d9796ad
wcy-cs <39185517+wcy-cs@users.noreply.github.com>
2023-05-30 11:13:47 +0800

    Create util.py

---
+++ utils/util.py
@@ -0,0 +1,25 @@
+import time
+class timer():
+    def __init__(self):
+        self.acc = 0
+        self.tic()
+
+    def tic(self):
+        self.t0 = time.time()
+
+    def toc(self, restart=False):
+        diff = time.time() - self.t0
+        if restart: self.t0 = time.time()
+        return diff
+
+    def hold(self):
+        self.acc += self.toc()
+
+    def release(self):
+        ret = self.acc
+        self.acc = 0
+
+        return ret
+
+    def reset(self):
+        self.acc = 0
